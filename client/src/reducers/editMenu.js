2c6fcebcce175a03bec83ab74ed8cccb04580e66
hayden7913 <hayden321@gmail.com>
2017-10-20 09:20:45 -0700

    moved clickedTaskId reducer to editMenu

--- client/src/reducers/editMenu.js
+++ client/src/reducers/editMenu.js
@@ -2,14 +2,21 @@ import * as actions from '../actions/indexActions';
 
 const defaultState = {
   activeParentId: null,
+  clickedTaskId: null,
 };
 
 export function editMenu(state = defaultState, action) {
   switch (action.type) {
     case actions.CHANGE_ACTIVE_EDIT_MENU:
       return {
+        ...state,
         activeParentId: action.parentId,
       };
+    case actions.TOGGLE_EDIT_TASK_FORM:
+      return {
+        ...state,
+       clickedTaskId: action.taskId,
+      }
     default:
       return state;
   }
--- client/src/reducers/indexReducer.js
+++ client/src/reducers/indexReducer.js
@@ -1,23 +1,19 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
-import { clickedTaskId } from './clickedTaskId';
 import { config } from './config';
 import { customForm } from './customForm';
 import { editMenu } from './editMenu';
 import { modal } from './modal';
 import { projects } from './projects';
-import { selectedProjectId } from './selectedProjectId';
 import { timer } from './timer';
 
 export default combineReducers({
-  clickedTaskId,
   config,
   customForm,
   editMenu,
   modal,
   projects,
-  // selectedProjectId,
   timer,
   form: formReducer,
 });
