248648965dd52647082bce0bd7a8945627727b27
hayden7913 <hayden321@gmail.com>
2017-10-27 10:23:41 -0700

    refactoring one line arrow functions

--- server.js
+++ server.js
@@ -1,104 +1,79 @@
-const shouldDeleteDb = false;
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const app = express();
-
-// const shouldResetDb = false;
-
 const { PORT, DATABASE_URL } = require('./server-files/config');
-const { Projects } = require('./server-files/models');
-// const { sampleData } = require('./server-files/sampleData');
-
 const projectRouter = require('./server-files/routes/projectRouter');
 const taskRouter = require('./server-files/routes/taskRouter');
 
 mongoose.Promise = global.Promise;
+const app = express();
+const shouldDeleteDb = false;
+
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+  app.use(express.static('client/build'));
 }
 
-app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 projectRouter.use('/:id/tasks', taskRouter);
 app.use('/projects', projectRouter);
 app.use('*', (req, res) => {
-    res.status(404).json({ message: 'Not Found' });
+  res.status(404).json({ message: 'Not Found' });
 });
 
-//     const seedData = sampleData.projects;
-//
-//     return Projects.insertMany(seedData);
-// };
-// function resetDb() {
-//     return new Promise((resolve, reject) => {
-//         console.warn('Resetting database');
-//         mongoose.connection.dropDatabase()
-//     .then((result) => {
-//         seedSampleData();
-//         resolve(result);
-//     })
-//     .catch((err) => { return reject(err); });
-//     });
-// }
 function tearDownDb() {
-    return new Promise((resolve, reject) => {
-        console.warn('Deleting database');
-        mongoose.connection.dropDatabase()
-      .then((result) => { return resolve(result); })
-      .catch((err) => { return reject(err); });
-    });
+  return new Promise((resolve, reject) => {
+    console.warn('Deleting database');
+    mongoose.connection.dropDatabase()
+      .then(result => resolve(result))
+      .catch(err => reject(err));
+  });
 }
 
-// if (shouldResetDb === true) {
-//     resetDb();
-// }
-
 if (shouldDeleteDb === true) {
-    tearDownDb();
+  tearDownDb();
 }
 
 let server;
 
 function runServer(databaseUrl = DATABASE_URL, port = PORT) {
-    return new Promise((resolve, reject) => {
-        mongoose.connect(databaseUrl, (err) => {
-            if (err) {
-                return reject(err);
-            }
-            server = app.listen(port, () => {
-                console.log(`Your app is listening on port ${port}`);
-                resolve();
-            })
-      .on('error', (err) => {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, (err) => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', (err) => {
           mongoose.disconnect();
           reject(err);
-      });
         });
     });
+  });
 }
 
 function closeServer() {
-    return mongoose.disconnect().then(() => {
-        return new Promise((resolve, reject) => {
-            console.log('Closing server');
-            server.close((err) => {
-                if (err) {
-                    return reject(err);
-                }
-                resolve();
-            });
-        });
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close((err) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
     });
+  });
 }
 
 if (require.main === module) {
-    runServer().catch((err) => { return console.error(err); });
+  runServer().catch(err => console.error(err));
 }
 
 module.exports = { app, runServer, closeServer };
