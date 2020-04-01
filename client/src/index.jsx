db3a8f56b40c4669cec701029563040afaeda574
hayden7913 <hayden321@gmail.com>
2020-04-01 12:09:01 -0700

    Changes component extenstions from "js" -> "jsx"

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { IndexRoute, Router, Route, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
+import TagManager from 'react-gtm-module'
+import ReactGA from 'react-ga';
 
 import store from './redux-files/store';
 
@@ -15,6 +17,14 @@ import TimerPage from './containers/TimerPage';
 import './helpers/polyfill';
 import './styles/index.scss';
 import './styles/icons/icomoon/style.css';
+
+if (process.env.NODE_ENV === 'production') {
+  TagManager.initialize({
+    gtmId: 'GTM-KJWS7WT'
+  });
+
+  ReactGA.initialize('UA-160393427-3');
+}
 
 render((
   <Provider store={store}>
