d0ada85d9f7e4dd5d9fbdc2b214337bb5ac59479
hayden7913 <hayden321@gmail.com>
2020-04-05 20:20:24 -0700

    Upgrades to React 16; Moves react-inline-edit component from node_modules to src

--- client/src/containers/EditInlineText.jsx
+++ client/src/containers/EditInlineText.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import InlineEdit from 'react-edit-inline';
+import InlineEdit from '../components/InlineEdit';
 
 import { timeStringToSeconds } from '../helpers/time';
 
