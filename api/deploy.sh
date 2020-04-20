b6588c0eaa35b749ea4eb39ce26181355b15492c
hayden7913 <hayden321@gmail.com>
2020-04-19 20:50:49 -0700

    Adds pm2 config

---
+++ api/deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+REMOTE_APP_PATH=/home/ubuntu/portfolio/postgres-pomtracker/api
+
+if [[ -z "$1" ]]; then
+  echo "Please enter a commit message"
+  exit 1
+fi
+
+if [[ "$1" != "--skip-commit" ]]; then
+    git add . &&
+    git commit -m "$1"
+fi
+
+git push origin $CURRENT_BRANCH
+
+ssh -A -i ~/.ssh/MyKeyPair.pem ubuntu@$EC2IP4 /bin/bash <<EOF
+    cd $REMOTE_APP_PATH &&
+    git fetch origin $CURRENT_BRANCH
+    git checkout $CURRENT_BRANCH
+    git pull origin $CURRENT_BRANCH &&
+#    yarn &&
+#    pm2 stop ecosystem.config.js --env production
+#    pm2 start ecosystem.config.js --env production
+#    pm2 save
+EOF
